c5db7b70eee66faa45bcd27c6ec70e72cb6ae188
Dagalaev Istamal <i.dagalaev@vkteam.ru>
2025-04-09 09:05:19 +0300

    feat: Sing in & Sign up

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { Pool } from 'pg';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors({
+  origin: '*',
+  credentials: true,
+}));
+
 app.use(express.json());
 
 const pool = new Pool({
@@ -15,9 +24,66 @@ const pool = new Pool({
   database: process.env.DB_NAME,
 });
 
-app.get('/quotes', async (_req, res) => {
-  const { rows } = await pool.query('SELECT * FROM quotes');
-  res.json(rows);
+app.get('/quotes', async (req: any, res: any) => {
+  const authHeader = req.headers.Authorization;
+  if (!authHeader) {
+    return res.status(403).json({ message: 'Authorization required'});
+  }
+
+  const token = authHeader.split(' ')[1];
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+    const { rows } = await pool.query('SELECT * FROM quotes');
+    return res.json(rows);
+  } catch (err) {
+    res.status(403).json({ message: 'Invalid or expired token'});
+  }
+});
+
+// new users sign up
+app.post('/register', async (req: any, res: any) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username and pasword are required"})
+  }
+
+  const hashedPasword = await bcrypt.hash(password, 10);
+
+  try {
+    const result = await pool.query(
+      'INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id, username',
+      [username, hashedPasword]
+    );
+    console.log(result);
+    const user = result.rows[0];
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET!, { expiresIn: '1h' });
+    res.status(201).json({ token });
+  } catch (error) {
+    res.status(500).json('Error creating user');
+  }
+});
+
+app.post('/login', async (req: any, res: any) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(401).json('Username and password are requred');
+  }
+
+  const result = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+  const user = result.rows[0];
+  if (!user) {
+    return res.status(400).json({ message: 'Invalid credentials'});
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(400).json({ message: 'Invalid credentials'});
+  }
+
+  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET!, { expiresIn: '1h' });
+  res.json({ token });
 });
 
 app.listen(4000, () => {
--- frontend-web/src/App.jsx
+++ frontend-web/src/App.jsx
@@ -1,7 +1,45 @@
 import React, { useEffect, useState } from 'react';
 
 const App = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState('');
   const [quotes, setQuotes] = useState([]);
+
+  const handleLogin = async () => {
+    const response = await fetch('http://localhost:4000/login', {
+      method: 'Post',
+      headers: { 'Content-Type': 'aplication/json' },
+      body: JSON.stringify({ username, password})
+    });
+
+    const data = await response.json();
+    if (data.token) {
+      setToken(data.token);
+      setIsLoggedIn(true);
+      localStorage.setItem('token', data.token);
+    } else {
+      alert('Invalid credentials');
+    }
+  };
+
+  const handleRegister = async () => {
+    const response = await fetch('http://localhost:4000/register', {
+      method: 'POST',
+      headers: { 'Content-Type' : 'application/json' },
+      body: JSON.stringify({ username, password })
+    })
+
+    const data = await response.json();
+    if (data.token) {
+      setToken(data.token);
+      setIsLoggedIn(true);
+      localStorage.setItem('token', data.token);
+    } else {
+      alert('Register failed');
+    }
+  }
 
   useEffect(() => {
     fetch('http://localhost:4000/quotes')
@@ -12,11 +50,32 @@ const App = () => {
   return (
     <div>
       <h1>Word Resonance</h1>
-      <ul>
-        {quotes.map(quote => (
-          <li key={quote.id}>{quote.text}</li>
-        ))}
-      </ul>
+      {isLoggedIn ? (
+        <>
+          <h2>Wellcome, {username}</h2>
+          <ul>
+            {quotes.map(quote => (
+              <li key={quote.id}>{quote.text}</li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <>
+          <input
+            type='text'
+            placeholder='Username'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type='password'
+            placeholder='Password'
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleLogin}>Sign In</button>
+          <button onClick={handleRegister}>Sign Up</button>
+        </>
+      )}
     </div>
   );
 };
